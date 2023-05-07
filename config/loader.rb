971b30c65687210aa3c8b507e6e79d115c6e1987
Nacho Morales <moralesignaciojoaquin@gmail.com>
2023-05-07 12:44:40 -0300

    Refactor the constants loading logic

--- config/loader.rb
+++ config/loader.rb
@@ -1,20 +1,37 @@
 require 'zeitwerk'
 
 class Loader
-  attr_accessor :loader
+  attr_accessor :loader, :env
+
+  TEST_ONLY = %w[spec].freeze
+  DEFAULT = %w[app app/models app/controllers lib].freeze
 
   def initialize(**kwargs)
     @loader = Zeitwerk::Loader.new
-    setup_paths(kwargs[:paths])
+    @env = kwargs[:env]
   end
 
-  def setup
+  def load
+    attach_loader_paths
     loader.setup
+  end
+
+  def self.load(**kwargs)
+    new(**kwargs).load
   end
 
   private
 
-  def setup_paths(paths)
+  def attach_loader_paths
     paths.each { |p| loader.push_dir(File.expand_path(p)) }
+  end
+
+  def paths
+    @paths ||= begin
+      paths = []
+      paths << DEFAULT
+      paths << TEST_ONLY if env == 'test'
+      paths.flatten
+    end
   end
 end
